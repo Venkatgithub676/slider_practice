e9532e21ced7181a0352a9ccd1dc19edb88fce93
Venkat <venkatgithub676@gmail.com>
2024-12-10 19:39:39 +0000

    Last Commit

--- src/components/PlanetItem/index.js
+++ src/components/PlanetItem/index.js
@@ -1,8 +1,19 @@
 // Write your code here
+import './index.css'
 
 const PlanetItem = props => {
   const {each} = props
-  return <h1>Venkatesh</h1>
+  return (
+    <div className="image-con">
+      <img
+        className="image-url"
+        alt={`planet ${each.name}`}
+        src={each.imageUrl}
+      />
+      <h1 className="name">{each.name}</h1>
+      <p className="description">{each.description}</p>
+    </div>
+  )
 }
 
 export default PlanetItem
--- src/components/PlanetsSlider/index.js
+++ src/components/PlanetsSlider/index.js
@@ -1,15 +1,21 @@
 // Write your code here
 
 import Slider from 'react-slick'
-import {Chrono} from 'react-chrono'
+
 import './index.css'
 import PlanetItem from '../PlanetItem'
 
+const settings = {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+}
 const PlanetsSlider = props => {
   const {planetsList} = props
   return (
-    <div className="slider-con">
-      <Slider>
+    <div className="slider-con" data-testid="planets">
+      <h1 className="planets">PLANETS</h1>
+      <Slider {...settings} className="slider-main-con">
         {planetsList.map(each => (
           <PlanetItem each={each} key={each.id} />
         ))}
